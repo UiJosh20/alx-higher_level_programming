9ceaf6b3c8a8fa74400fc951a74004b1513d0491
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-08 13:45:20 -0700

    A raised to the power of B

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/urs/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-        return result
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
+    elif b % 2 == 0:
+        temp = pow(a, b // 2)
+        return temp * temp
+    else:
+        return a * pow(a, b - 1)
