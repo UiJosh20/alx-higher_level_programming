845f10771e55b254367ee63b5655532d72d9770d
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-11 13:02:20 -0700

    add number

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
+a = 1
+b = 2
+
+if __name__ =="__main__":
+    from add_0 import add
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
+
