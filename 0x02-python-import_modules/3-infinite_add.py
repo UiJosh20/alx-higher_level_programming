dccb4da954a8eb09795097325511b5f6ac20a597
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-11 14:08:11 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     result = 0
-    for args in sys.argv[1:]:
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
             result += int(arg)
     print(result)
