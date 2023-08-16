af9e644ff11d06ce9744324d453fde09e2037853
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-16 12:19:25 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,6 @@ def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
     highest = max(a_dictionary.values())
-    for kee, value in a_dictionary.items():
+    for key, value in a_dictionary.items():
         if value is highest:
-    return kee
+            return key
