78f0a43f0dd71a7db106c5a416ab4e46d43b8618
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-29 10:01:46 -0700

    square 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-class square:
-    def __init__(self, side_length):
-        self.side_length = side_length
-
-    def area(self):
-        return self.side_length ** 2
-    def perimeter(self):
-        return 4 * self.side_length
+class Square:
+    
+    pass
